708b2e7e13ede5d5a9d5dcf005d8ef51a7dad84d
Duong Nguyen <duongital@gmail.com>
2020-06-04 14:47:58 +0700

    call api

--- src/my-element.js
+++ src/my-element.js
@@ -1,70 +1,105 @@
-import { LitElement, html } from 'lit-element';
+import { LitElement, css, html } from "lit-element";
 
 class MyElement extends LitElement {
-  static get properties() { return {
-    prop1: { type: String, reflect: true },
-    prop2: { type: Number, reflect: true },
-    prop3: { type: Boolean, reflect: true },
-    prop4: { type: Array, reflect: true },
-    prop5: { type: Object, reflect: true }
-  };}
+  static get properties() {
+    return {
+      prop1: { type: String, reflect: true },
+      prop2: { type: Number, reflect: true },
+      prop3: { type: Boolean, reflect: true },
+      prop4: { type: Array, reflect: true },
+      prop5: { type: Object, reflect: true },
+      prop6: { type: Array, reflect: true },
+    };
+  }
+
+  static get styles() {
+    // Write styles in standard CSS
+    return css`
+      .green {
+        margin-left: 100px;
+      }
+    `;
+  }
 
   constructor() {
     super();
-    this.prop1 = '';
+    this.prop1 = "";
     this.prop2 = 0;
     this.prop3 = false;
     this.prop4 = [];
-    this.prop5 = { };
+    this.prop5 = {};
+    this.prop6 = {};
   }
 
   attributeChangedCallback(name, oldVal, newVal) {
-    console.log('attribute change: ', name, newVal);
+    console.log("attribute change: ", name, newVal);
     super.attributeChangedCallback(name, oldVal, newVal);
+  }
+
+  action() {
+    console.log("hello world");
+    fetch("https://jsonplaceholder.typicode.com/todos/1")
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json);
+        this.prop6 = json;
+      });
   }
 
   render() {
     return html`
-      <p>prop1 ${this.prop1}</p>
+      <p style="color: red">prop1 ${this.prop1}</p>
       <p>prop2 ${this.prop2}</p>
       <p>prop3 ${this.prop3}</p>
 
-      <p>prop4: ${this.prop4.map((item, index) =>
-        html`<span>[${index}]:${item}&nbsp;</span>`)}
+      <p>
+        prop4:
+        ${this.prop4.map(
+          (item, index) => html`<span>[${index}]:${item}&nbsp;</span>`
+        )}
       </p>
 
-      <p>prop5:
-        ${Object.keys(this.prop5).map(item =>
-          html`<span>${item}: ${this.prop5[item]}&nbsp;</span>`)}
+      <span part="box" class="${this.prop1}">span is green ${this.prop1}</span>
+
+      <p>
+        prop5:
+        ${Object.keys(this.prop5).map(
+          (item) => html`<span>${item}: ${this.prop5[item]}&nbsp;</span>`
+        )}
       </p>
+
+      <p>prop6 with calling api: ${this.prop6.title}</p>
 
       <button @click="${this.changeProperties}">change properties</button>
       <button @click="${this.changeAttributes}">change attributes</button>
+      <button @click="${this.action}">action</button>
     `;
   }
 
   changeAttributes() {
-    let randy = Math.floor(Math.random()*10);
-    let myBool = this.getAttribute('prop3');
+    let randy = Math.floor(Math.random() * 10);
+    let myBool = this.getAttribute("prop3");
 
-    this.setAttribute('prop1', randy.toString());
-    this.setAttribute('prop2', randy.toString());
-    this.setAttribute('prop3', myBool? '' : null);
-    this.setAttribute('prop4', JSON.stringify([...this.prop4, randy]));
-    this.setAttribute('prop5',
-      JSON.stringify(Object.assign({}, this.prop5, {[randy]: randy})));
+    this.setAttribute("prop1", randy.toString());
+    this.setAttribute("prop2", randy.toString());
+    this.setAttribute("prop3", myBool ? "" : null);
+    this.setAttribute("prop4", JSON.stringify([...this.prop4, randy]));
+    this.setAttribute(
+      "prop5",
+      JSON.stringify(Object.assign({}, this.prop5, { [randy]: randy }))
+    );
     this.requestUpdate();
   }
 
   changeProperties() {
-    let randy = Math.floor(Math.random()*10);
+    let randy = Math.floor(Math.random() * 10);
     let myBool = this.prop3;
 
     this.prop1 = randy.toString();
     this.prop2 = randy;
     this.prop3 = !myBool;
     this.prop4 = [...this.prop4, randy];
-    this.prop5 = Object.assign({}, this.prop5, {[randy]: randy});
+    this.prop5 = Object.assign({}, this.prop5, { [randy]: randy });
   }
 
   updated(changedProperties) {
@@ -72,7 +107,6 @@ class MyElement extends LitElement {
       console.log(`${propName} changed. oldValue: ${oldValue}`);
     });
   }
-
 }
 
-customElements.define('my-element', MyElement);
+customElements.define("my-element", MyElement);
